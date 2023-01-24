869f5095e174be69709c79739d4509a0dd9d20c4
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-24 12:11:40 +0100

    desctructure item from data file

--- src/Menu.js
+++ src/Menu.js
@@ -4,8 +4,11 @@ const Menu = ({menuData}) => {
     return (
         <div className="section-center">
             {menuData.map((item) => {
+                const {id, title, img, price, desc} = item
                 return (
-                    
+                   <article className="menu-item">
+                    <img src={img} alt={title} className="phot" />
+                   </article> 
                 )
             })}
         </div>
