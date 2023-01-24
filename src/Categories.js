ee3cfe2c65bd18578f8f1c79cd114cec0f6f9dc3
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-24 12:22:46 +0100

    test menu functionality

--- src/Categories.js
+++ src/Categories.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Categories = ({filterItems}) => {
   return (
-    
+    <div className="btn-container">
+      <button className='filter-btn' onClick={() => filterItems('breakfast')}>
+        Breakfast
+      </button>
+    </div>
   )
 }
 
